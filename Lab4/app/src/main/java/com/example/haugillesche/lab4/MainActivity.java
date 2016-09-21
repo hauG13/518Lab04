26ed7a94eaf7b0501112fed3b6592238bb928f24
Hau Gilles Che <hau.che@dawsoncollege.qc.ca>
2016-09-21 17:16:55 -0400

    lab4 2 languages implemented

---
+++ Lab4/app/src/main/java/com/example/haugillesche/lab4/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.haugillesche.lab4;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    public void showMap(View v)
+    {
+
+        Uri geoLocation = Uri.parse("geo:0,0?q=" + Uri.encode(getString(R.string.hello)));
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+
+
+
+
+}
+
